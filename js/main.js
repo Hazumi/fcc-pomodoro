3cff280c0c6a4a6acbffc5a576d24d94e2168456
hazumi <shirohazumi@openmailbox.org>
2017-01-17 15:52:16 -0500

    implemented pause

--- js/main.js
+++ js/main.js
@@ -1,25 +1,39 @@
-const timer = document.getElementById('timer');
+const $timer = document.getElementById('timer');
+const $duration = document.getElementById('duration');
+
+let duration = document.getElementById('duration').value
+let paused = false;
+
+duration = 4 * 60; // 4 minutes
+duration = 3;
 
 const pomodoro = new Timer({
-    tick : 1,
-    ontick : function (sec) {
-        timer.innerHTML = Math.round(sec / 1000);
+    tick: 1,
+    ontick: function (sec) {
+        $timer.innerHTML = Math.round(sec/1000);
         console.log('interval', sec);
     },
-    onstart : function() {
+    onstart: function() {
         console.log('STARTING');
+    },
+    onpause: function() {
+        console.log('paused');
     }
 });
 
-let duration = 3;
-
-let pomoTime = 10;
-
-// defining options using on
-pomodoro.start(pomoTime).on('end', function () {
-
-    console.log('timer ended');
-    // this.start(4).off('end');
+$timer.addEventListener('click', function() {
+    if (paused) {
+        pomodoro.start();
+        paused = false;
+    } else {
+        pomodoro.pause();
+        paused = true;
+    }
 });
 
-//start timer for 10 seconds
+// defining options using on
+pomodoro.start(duration).on('end', function () {
+    //$timer.innerHTML = "0";
+    this.start(4).off('end');
+});
+
